117c0ab4df4c6970aa64fd22b55fe3b51cdc44a1
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-06-16 13:37:03 +0200

    inizialize vue , creat app , set data and mount in html

--- js/script.js
+++ js/script.js
@@ -2,3 +2,18 @@
 console.log('ok js');
 console.log('ok vue', Vue);
 
+//estrapolo create app
+const { createApp } = Vue;
+
+//inzializzo l app
+const app = createApp({
+    data(){
+        return{
+            name : 'Riccardo',
+            image : 'https://seeklogo.com/images/O/one-piece-logo-3ECB90CB5A-seeklogo.com.png'
+        }
+    },
+});
+
+//monto nel html
+app.mount('#root');
