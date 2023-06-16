e824df0194572d7e12dab47fd61ac4c95a5e41f1
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-06-16 14:00:17 +0200

    add methods

--- js/script.js
+++ js/script.js
@@ -9,11 +9,22 @@ const { createApp } = Vue;
 const app = createApp({
     data(){
         return{
-            name : 'Riccardo',
-            image : 'https://seeklogo.com/images/O/one-piece-logo-3ECB90CB5A-seeklogo.com.png'
+            name : '',
+            image : 'https://i.pinimg.com/originals/e9/91/80/e991804378163190adbd288a473edc85.jpg'
         }
     },
+
+    methods: {
+        choosePic(){
+            if(this.name === 'Joy Boy'){
+                this.image = 'https://seeklogo.com/images/O/one-piece-logo-3ECB90CB5A-seeklogo.com.png'
+            } else if (this.name === 'Luffy'){
+                this.image = 'https://i.pinimg.com/474x/95/52/7d/95527d44fe4e92a90a49a89408b75788.jpg'
+            }
+        }
+    }
 });
 
 //monto nel html
 app.mount('#root');
+
